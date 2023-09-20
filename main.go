c9095fd2cbf442ec5fde04ca8808eb9c7ff996a9
DevitoDbug <Davidochiengy@gmail.com>
2023-09-20 21:46:04 +0300

    Create validateWord functionality

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+)
+
+func validateWord(url string, payload []byte) (*http.Response, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func main() {
+
+}
