1a47707e8c4de1d1a262b9095793a198eb23fc0c
DevitoDbug <Davidochiengy@gmail.com>
2023-09-21 06:37:30 +0300

    Receive command line args

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func validateWord(url string, payload []byte) (*http.Response, error) {
@@ -10,6 +13,13 @@ func validateWord(url string, payload []byte) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}(req.Body)
 
 	req.Header.Set("Content-Type", "application/json")
 
@@ -22,5 +32,11 @@ func validateWord(url string, payload []byte) (*http.Response, error) {
 }
 
 func main() {
+	var commitMessage []string
+	//url := "https://words.dev-apis.com/validate-word"
 
+	if len(os.Args) >= 2 {
+		commitMessage = append(commitMessage, os.Args[1:]...)
+	}
+	fmt.Println(commitMessage)
 }
